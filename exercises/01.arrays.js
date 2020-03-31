8b416ab37db7234cb104b684a26c61efe8825ac7
mlmcgrath1925 <mmcgrath19@gmail.com>
2020-03-31 13:30:20 -0400

    Testing Notes

--- exercises/01.arrays.js
+++ exercises/01.arrays.js
@@ -10,16 +10,24 @@
  * @description
  * create an arrays of names
  */
-
+var siblings = ['Jami', 'Jodi', 'Juli']
+console.log(siblings)
+console.log(siblings.length)
 /**
  * @description
  * access the first item in the array above
  */
+//What I originally did..
+//var first = siblings[0]
+//console.log(first)
 
+console.log(siblings[0])
 /**
  * @description
  * access the last character in the first item in the array above
  */
+var letter = siblings[0][siblings[0].length - 1]
+console.log(letter)
 
 /**
  * @do
@@ -27,9 +35,9 @@
  */
 
 
- git add . 
- git commit -m ' ... '
- git pull upstream master
+// git add . 
+// git commit -m ' ... '
+// git pull upstream master
 
 
  
